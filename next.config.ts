a1aaa2441b7f74213b8bad2e91437d402a850627
Hiroto Shinkawa <84663648+iharashuuji@users.noreply.github.com>
2025-11-21 08:07:53 +0900

    next.config.ts を更新

--- next.config.ts
+++ next.config.ts
@@ -7,8 +7,8 @@ const nextConfig = {
     NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     // (ミドルウェアで非公開キーを使う場合は以下も追加)
-    // SUPABASE_URL: process.env.SUPABASE_URL,
-    // SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
   },
 };
 
